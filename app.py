e8bdfab505f4167909b69bd6f552c58b90c2d0a0
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2017-10-16 20:57:29 -0500

    create, read and delete users from database. More templates.

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, session, abort
+from flask_pymongo import PyMongo, ObjectId
+import os
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
+def index():
+    users = mongo.db.users.find()
+    if not session.get('logged_in'):
+        return render_template('login.html', title="Please log in")
+    else:
+        return render_template('index.html', title="Welcome", users=users)
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['field-password'] == 'password' and request.form['field-email'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return index()
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session['logged_in'] = False
+    return index()
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title="Please fill out the form")
+
+@app.route('/createuser')
+def createuser():
+    return register()
+
+@app.route('/newuser', methods=['POST'])
+def newuser():
+    if request.form['field-password'] != '' and request.form['field-email'] != '':
+        mongo.db.users.insert_one({
+            'email': request.form['field-email'],
+            'password': request.form['field-password'],
+            'namefirst': request.form['field-fname'],
+            'namelast': request.form['field-lname'],
+            'age': request.form['field-age']
+        })
+
+    return index()
+
+@app.route('/removeuser', methods=['POST'])
+def removeuser():
+    mongo.db.users.delete_one({'_id': ObjectId(request.form['radio-delete'])})
+    return index()
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
